e8f65a382d3f3d0b23d3d86ef01f19bfe0ec8fdc
krzyb5081 <krzysztof.sobczak5081@gmail.com>
2024-05-26 23:41:27 +0200

    Controller update

--- src/main/java/com/krzyb5081/app/controller/ViewController.java
+++ src/main/java/com/krzyb5081/app/controller/ViewController.java
@@ -2,6 +2,7 @@ package com.krzyb5081.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ViewController {
@@ -9,5 +10,15 @@ public class ViewController {
 	@GetMapping("/")
 	public String react() {
 		return "index";
+	}
+	
+	@GetMapping("/showReports")
+	public String showReports() {
+		return "showReports";
+	}
+	
+	@PostMapping("/createReport")
+	public String createReport() {
+		return "createReport";
 	}
 }
