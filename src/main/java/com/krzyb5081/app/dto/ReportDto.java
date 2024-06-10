b10e55c993c2e738bcc18fba2deb54a020a3fff9
krzyb5081 <krzysztof.sobczak5081@gmail.com>
2024-06-11 00:00:21 +0200

    update

---
+++ src/main/java/com/krzyb5081/app/dto/ReportDto.java
@@ -0,0 +1,17 @@
+package com.krzyb5081.app.dto;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Component
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class ReportDto {
+
+}
