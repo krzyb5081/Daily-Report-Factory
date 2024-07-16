5a630f0769480005087fbaf95d52947cec74f63b
krzyb5081 <krzysztof.sobczak5081@gmail.com>
2024-07-16 19:44:36 +0200

    simple pdf opening :)

---
+++ pdfeditor/src/main/java/com/editor/Main.java
@@ -0,0 +1,20 @@
+package com.editor;
+
+import java.io.IOException;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		System.out.print("hello");
+	    PdfReader reader = new PdfReader("src/main/resources/template.pdf");
+	    PdfWriter writer = new PdfWriter("src/main/resources/template-modified.pdf");
+	    PdfDocument pdfDocument = new PdfDocument(reader, writer);
+	    pdfDocument.close();
+
+	}
+
+}
