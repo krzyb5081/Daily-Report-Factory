8038d1e4db352af55814a6a3696dccb13319f621
krzyb5081 <krzysztof.sobczak5081@gmail.com>
2024-08-06 21:41:57 +0200

    Doing simple things with template.pdf

--- pdfeditor/src/pdfEditor/Main.java
+++ pdfeditor/src/pdfEditor/Main.java
@@ -1,12 +1,18 @@
 package pdfeditor;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.print("hello");
-		PDDocument hello;
+	public static void main(String[] args) throws IOException {
+		File template = new File("src/pdfeditor/template.pdf");
+		PDDocument document = PDDocument.load(template);
+		document.getEncryption();
+		System.out.print("Number of pages: " + document.getNumberOfPages());
+		document.close();
 	}
 
 }
