c40ec75adbd8f1b018acf1b465d209abb80f29dd
krzyb5081 <krzysztof.sobczak5081@gmail.com>
2024-08-06 21:24:12 +0200

    PDF editor module works

---
+++ pdfeditor/src/pdfEditor/Main.java
@@ -0,0 +1,12 @@
+package pdfeditor;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.print("hello");
+		PDDocument hello;
+	}
+
+}
