4bd3c670ed660e68ac37c05a5a6cc10ce4060699
krzyb5081 <krzysztof.sobczak5081@gmail.com>
2024-08-07 22:50:50 +0200

    Finding plain text

--- pdfeditor/src/pdfEditor/Main.java
+++ pdfeditor/src/pdfEditor/Main.java
@@ -4,14 +4,19 @@ import java.io.File;
 import java.io.IOException;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		File template = new File("src/pdfeditor/template.pdf");
 		PDDocument document = PDDocument.load(template);
-		document.getEncryption();
-		System.out.print("Number of pages: " + document.getNumberOfPages());
+		
+		PDFTextStripper stripper = new PDFTextStripper();
+		
+		
+		
+		System.out.print(stripper.getText(document).toString());
 		document.close();
 	}
 
