bb49615cdd8343ec095225fc6512f9fa287e5bb0
krzyb5081 <krzysztof.sobczak5081@gmail.com>
2025-06-06 01:56:53 +0200

    Login with spring security with database - service todo

--- src/main/java/com/krzyb5081/app/service/UserDetailsServiceImpl.java
+++ src/main/java/com/krzyb5081/app/service/UserDetailsServiceImpl.java
@@ -1,17 +1,22 @@
 package com.krzyb5081.app.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.krzyb5081.app.repository.UserRepository;
+
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+	@Autowired
+	UserRepository userRepo;
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
+			userRepo.findByUsername(username).orElseThrow().getRole();
 		return null;
 	}
 
